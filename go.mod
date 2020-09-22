e783cf822050f7835cf8f30df20fb9f6f7a98c44
txya900619 <wayne900619@gmail.com>
2020-09-22 12:26:39 +0800

    delete entroy part

--- go.mod
+++ go.mod
@@ -3,14 +3,23 @@ module Mr.Coding-LineBot
 go 1.15
 
 require (
-	cloud.google.com/go v0.65.0 // indirect
+	cloud.google.com/go v0.66.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-playground/validator/v10 v10.3.0
-	github.com/line/line-bot-sdk-go v7.5.0+incompatible
+	github.com/line/line-bot-sdk-go v7.6.0+incompatible
+	github.com/magiconair/properties v1.8.3 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/spf13/afero v1.4.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.1
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	golang.org/x/sys v0.0.0-20200831180312-196b9ba8737a // indirect
-	google.golang.org/api v0.30.0
-	google.golang.org/genproto v0.0.0-20200831141814-d751682dd103 // indirect
-	google.golang.org/grpc v1.31.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
+	google.golang.org/api v0.32.0
+	google.golang.org/genproto v0.0.0-20200921165018-b9da36f5f452 // indirect
+	google.golang.org/grpc v1.32.0 // indirect
+	gopkg.in/ini.v1 v1.61.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"Mr.Coding-LineBot/config"
-	"Mr.Coding-LineBot/entroy"
-	"Mr.Coding-LineBot/mrcoding"
-	"Mr.Coding-LineBot/spreadsheets"
 	"fmt"
-	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
 	"net/http"
+
+	"Mr.Coding-LineBot/config"
+	"Mr.Coding-LineBot/mrcoding"
+	"Mr.Coding-LineBot/spreadsheets"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 var bot *mrcoding.Bot
-var entroyBot *entroy.Bot
 
 func main() {
 	// Get config at ./config.yaml
@@ -25,7 +24,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Create linebot fail, %v", err)
 	}
-	entroyBot = entroy.New(bot.Client)
 
 	http.HandleFunc("/callback", callbackHandler)
 	err = http.ListenAndServe(":1225", nil)
@@ -47,168 +45,105 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, event := range events {
-
-		if entroyBot.CheckPlayerInGame(event.Source.UserID) {
-			switch event.Type {
-			case linebot.EventTypeMessage:
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if entroy.CheckAnswerText(message.Text) {
-						player := entroyBot.PlayerList[event.Source.UserID]
-
-						currentQuestion := entroy.Questions[player.AnsweredList[len(player.AnsweredList)-1]]
-
-						if len(player.AnsweredList) == 5 {
-							currentQuestion.Final = true
-						}
-
-						if message.Text == currentQuestion.Answer {
-							player.Score++
-						}
-
-						entroyBot.ReplyMessage(event.ReplyToken, currentQuestion.ReasonMessage(message.Text)).Do()
-						return
-
-					} else if message.Text == "社團博覽會有獎徵答" {
-						entroyBot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(`不要再輸入 社團博覽會有獎徵答 拉 > <`)).Do()
-						return
-					} else {
-						entroyBot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("要輸入 1 到 4 歐")).Do()
-						return
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				switch message.Text {
+				case "Mr.Coding 表單":
+					//bot.ReplyMessage(event.ReplyToken, linebot.NewFlexMessage("working", mrcoding.GetWorkingFlexContainer())).Do()
+					//return
+					answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
+					if err != nil {
+						log.Fatal(err)
 					}
+					if answerRowID == 0 {
+						message, err := bot.QuestionStart(event.Source.UserID)
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						bot.ReplyMessage(event.ReplyToken, message).Do()
+					} else {
+						message, err := bot.SaveAnswerAndGetNextMessage(message.Text, answerRowID, event.Source.UserID)
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						bot.ReplyMessage(event.ReplyToken, message).Do()
+					}
+					return
+				case "社團博覽會有獎徵答":
+
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("社團博覽會已結束")).Do()
+					return
+				case "/help":
+					bot.ReplyMessage(event.ReplyToken, mrcoding.HelpMessage()).Do()
+					return
 				default:
-					entroyBot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("不要傳文字以外的訊息歐 ～～")).Do()
+					answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					if answerRowID != 0 {
+						message, err := bot.SaveAnswerAndGetNextMessage(message.Text, answerRowID, event.Source.UserID)
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						bot.ReplyMessage(event.ReplyToken, message).Do()
+					} else {
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("點開選單選擇功能，\n或輸入 /help 選擇想使用的功能。")).Do()
+					}
 					return
 				}
-			case linebot.EventTypePostback:
-				switch event.Postback.Data {
-				case "next":
-					player := entroyBot.PlayerList[event.Source.UserID]
-					if len(player.AnsweredList) >= 5 {
-						message := player.FinalMessage()
-						delete(entroyBot.PlayerList, event.Source.UserID)
-
-						entroyBot.ReplyMessage(event.ReplyToken, message).Do()
-						return
-					} else {
-						nextQuestionID := player.RandomQuestionID()
-						message := player.GetQuestionMessageByID(nextQuestionID)
-						player.AnsweredList = append(player.AnsweredList, nextQuestionID)
-
-						entroyBot.ReplyMessage(event.ReplyToken, message).Do()
-						return
-					}
+			case *linebot.ImageMessage:
+				answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
+				if err != nil {
+					log.Fatal(err)
 				}
+				questionColID, err := bot.Spreadsheets.FindCurrentQuestionColID(answerRowID)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if questionColID == spreadsheets.QuestionUploadFile {
+					content, err := bot.GetMessageContent(message.ID).Do()
+					if err != nil {
+						log.Fatal(err)
+					}
+					fileURL, err := bot.Drive.UploadNewFile(content.Content, event.Timestamp.String()+"-"+event.Source.UserID)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					message, err := bot.SaveAnswerAndGetNextMessage(fileURL, answerRowID, event.Source.UserID)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					bot.ReplyMessage(event.ReplyToken, message).Do()
+				} else {
+					bot.ReplyMessage(event.ReplyToken, linebot.NewFlexMessage("error", mrcoding.GetTypeErrorFlexContainer())).Do()
+				}
+				return
+
 			}
-		} else {
-			switch event.Type {
-			case linebot.EventTypeMessage:
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					switch message.Text {
-					case "Mr.Coding 表單":
-						//bot.ReplyMessage(event.ReplyToken, linebot.NewFlexMessage("working", mrcoding.GetWorkingFlexContainer())).Do()
-						//return
-						answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
-						if err != nil {
-							log.Fatal(err)
-						}
-						if answerRowID == 0 {
-							message, err := bot.QuestionStart(event.Source.UserID)
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							bot.ReplyMessage(event.ReplyToken, message).Do()
-							return
-						} else {
-							message, err := bot.SaveAnswerAndGetNextMessage(message.Text, answerRowID, event.Source.UserID)
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							bot.ReplyMessage(event.ReplyToken, message).Do()
-							return
-						}
-					case "社團博覽會有獎徵答":
-						player := &entroy.Player{
-							AnsweredList: make([]uint, 0),
-							Score:        0,
-						}
-						entroyBot.PlayerList[event.Source.UserID] = player
-
-						entroyBot.ReplyMessage(event.ReplyToken, entroy.StartMessage()).Do()
-						return
-					case "/help":
-						bot.ReplyMessage(event.ReplyToken, mrcoding.HelpMessage()).Do()
-						return
-					default:
-						answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						if answerRowID != 0 {
-							message, err := bot.SaveAnswerAndGetNextMessage(message.Text, answerRowID, event.Source.UserID)
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							bot.ReplyMessage(event.ReplyToken, message).Do()
-							return
-						} else {
-							bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("點開選單選擇功能，\n或輸入 /help 選擇想使用的功能。")).Do()
-							return
-						}
-					}
-				case *linebot.ImageMessage:
-					answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
-					if err != nil {
-						log.Fatal(err)
-					}
-					questionColID, err := bot.Spreadsheets.FindCurrentQuestionColID(answerRowID)
-					if err != nil {
-						log.Fatal(err)
-					}
-					if questionColID == spreadsheets.QuestionUploadFile {
-						content, err := bot.GetMessageContent(message.ID).Do()
-						if err != nil {
-							log.Fatal(err)
-						}
-						fileUrl, err := bot.Drive.UploadNewFile(content.Content, event.Timestamp.String()+"-"+event.Source.UserID)
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						message, err := bot.SaveAnswerAndGetNextMessage(fileUrl, answerRowID, event.Source.UserID)
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						bot.ReplyMessage(event.ReplyToken, message).Do()
-						return
-					} else {
-						bot.ReplyMessage(event.ReplyToken, linebot.NewFlexMessage("error", mrcoding.GetTypeErrorFlexContainer())).Do()
-						return
-					}
-
+		case linebot.EventTypePostback:
+			switch event.Postback.Data {
+			case "pass":
+				answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
+				if err != nil {
+					log.Fatal(err)
 				}
-			case linebot.EventTypePostback:
-				switch event.Postback.Data {
-				case "pass":
-					answerRowID, err := bot.Spreadsheets.FindAnswerRowID(event.Source.UserID)
+				if answerRowID != 0 {
+					message, err := bot.SaveAnswerAndGetNextMessage("NULL", answerRowID, event.Source.UserID)
 					if err != nil {
 						log.Fatal(err)
 					}
-					if answerRowID != 0 {
-						message, err := bot.SaveAnswerAndGetNextMessage("NULL", answerRowID, event.Source.UserID)
-						if err != nil {
-							log.Fatal(err)
-						}
 
-						bot.ReplyMessage(event.ReplyToken, message).Do()
-						return
-					}
+					bot.ReplyMessage(event.ReplyToken, message).Do()
+					return
 				}
 			}
 		}
